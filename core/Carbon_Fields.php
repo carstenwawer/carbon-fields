1820b7c1821e488910cae03028eb60ac4387a03c
Carsten Wawer <carsten@barracuda.de>
2019-01-11 00:38:09 +0100

    Update Carbon_Fields.php

--- core/Carbon_Fields.php
+++ core/Carbon_Fields.php
@@ -231,11 +231,11 @@ final class Carbon_Fields {
 		# Sanitize directory separator on Windows
 		$url = str_replace( '\\' ,'/', $url );
 
-		$possible_locations = array(
+		$possible_locations = apply_filters('carbon_fields_possible_locations', array(
 			WP_PLUGIN_DIR => \plugins_url(), # If installed as a plugin
 			WP_CONTENT_DIR => \content_url(), # If anywhere in wp-content
 			ABSPATH => \site_url( '/' ), # If anywhere else within the WordPress installation
-		);
+		));
 
 		foreach ( $possible_locations as $test_dir => $test_url ) {
 			$test_dir_normalized = str_replace( '\\' ,'/', $test_dir );
